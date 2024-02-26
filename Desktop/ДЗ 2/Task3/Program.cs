948802537e943c16adcb2389a0f03f7268ad17a2
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-02-26 13:11:29 +0300

    add Task3

---
+++ Desktop/ДЗ 2/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿int [] ArrayInput (int n)
+{
+    Console.WriteLine("Введите число от 1 до 9: ");
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray (int [] a)
+{
+    foreach (var item in a)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+double ArrayToNumber (int [] a)
+{
+    double s = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        double p = Math.Pow(10,a.Length-i-1);
+        s = s + a[i]*p;
+    }
+    return s;
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = ArrayInput(n);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(ArrayToNumber(array));
