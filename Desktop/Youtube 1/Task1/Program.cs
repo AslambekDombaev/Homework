e5d4ba6015533fa5d28ee3660c6fa11c1c7d256c
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-02-27 13:26:32 +0300

    Task1

---
+++ Desktop/Youtube 1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Enter name: ");
+string role = Console.ReadLine()!;
+
+if(role == "Admin") {
+Console.Write("Enter user name: ");
+string user_name = Console.ReadLine()!;
+Console.Write("Enter {0} age: ", user_name);
+short age = Convert.ToInt16(Console.ReadLine());
+
+if(age <= 0 || age > 99){
+Console.Write("Enter {0} age: ", user_name);
+age = Convert.ToInt16(Console.ReadLine());
+}
+
+if(age <= 0 || age > 99) {
+   Console.WriteLine("Error");
+    age = 10;
+}
+   else
+   Console.WriteLine("Users age is " + age);
+} else
+    Console.WriteLine("You are not admin!");
