8aeefb32392cd84113a9a8d307e57b890e53a04c
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-01 17:20:25 +0300

    Task1

---
+++ Desktop/Учеба/семинары/Функции/ДЗ/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] array = new int[8];
+int b = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Random rnd = new Random();
+    array[i] = rnd.Next(100, 1000);
+    if(array[i] % 2 == 0)
+    {
+        b = b + 1;
+    }
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine(b);
