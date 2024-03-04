5835c4a44b758283cc6875d73af3c0d007a1a2fa
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-04 13:46:20 +0300

    Task2

--- Desktop/Учеба/семинары/массивы&строки/Task2/Program.cs
+++ Desktop/Учеба/семинары/массивы&строки/Task2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На основе символов строки (тип данных string)
+// сформировать массив символов (тип char[]). 
+// Вывести массив на экран
+
+
+string imput = Console.ReadLine();
+
+char [] Mass_char = new char [imput.Length];
+
+for (int i = 0; i < imput.Length; i++)
+{
+    Mass_char[i] = imput[i];
+}
+
+void PrintChar(char[] Mass_char)
+{
+    foreach (char i in Mass_char)
+    {
+        Console.WriteLine(i);
+    }
+}
+PrintChar(Mass_char);
+
+
+// второе решение
+// char[] MassChar(string st)
+// {
+//     char[] Mass_char = new char[st.Length];
+//     for (int i = 0; i < st.Length; i++)
+//     {
+//         Mass_char[i] = st[i];
+//     }
+//     return Mass_char;
+// }
+
+// void PrintChar(char[] Mass_char)
+// {
+//     foreach (char i in Mass_char)
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+
+// string imput = Console.ReadLine()!;
+// char[] mass_char_new = MassChar(imput);
+// PrintChar(mass_char_new);
