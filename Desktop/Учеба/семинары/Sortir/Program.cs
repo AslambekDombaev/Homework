26107d2eeb416a8394da06151c6b6ec7e2fba206
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-04 13:04:28 +0300

    Sortir

---
+++ Desktop/Учеба/семинары/Sortir/Program.cs
@@ -0,0 +1,31 @@
+﻿void InputArray (int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(-20, 21); // [-20; 20]
+}
+
+int[] sortVibor (int[] array)
+{
+for (var i = 0; i < array.Length - 1; i++) // Счётчики цикла лучше всего обзывать i, j, k, m, n
+{
+int indexMin = i;
+for (var j = i + 1; j < array.Length; j++)
+{
+if(array[j] < array[indexMin])
+indexMin = j;
+}
+int temp = array[i];
+array[i] = array[indexMin];
+array[indexMin] = temp;
+}
+return array;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+// Для начала выведем на экран сгенерированные элементы
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", sortVibor(array))}]");
