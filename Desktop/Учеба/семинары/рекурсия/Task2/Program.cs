3f8d9b42dde307b8aebeae676bc5b8a2cfd0023c
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-07 13:49:36 +0300

    Task2

---
+++ Desktop/Учеба/семинары/рекурсия/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от 1 до N.
+
+// void PrintNum (int num)
+// {
+//     if (num < 1)
+//     {
+//         return;
+//     }
+//     Console.Write($"{num} ");
+//     PrintNum(num - 1);
+// }
+Console.WriteLine("Введите число равное M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+//PrintNum(n);
+
+string PrintNum(int n, int m)
+{
+    if (n == m)
+    {
+        return Convert.ToString(n);
+    }
+return n + " " + PrintNum(n + 1, m);
+}
+Console.WriteLine(PrintNum(1, m));
