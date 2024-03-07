fff22a56009367f816a86b300340186442052acf
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-07 14:15:35 +0300

    Task3

--- Desktop/Учеба/семинары/рекурсия/Task3/Program.cs
+++ Desktop/Учеба/семинары/рекурсия/Task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Считать строку с консоли, содержащую латинские буквы. 
+// Вывести на экран согласные буквы этой строки.
+
+void WriteConsonants (string n)
+{
+    if (n.Length == 0)
+    {
+        return;
+    }
+    string vowels = "euioaEUIOA";
+    if (!vowels.Contains(n[0]))
+    {
+        Console.Write(n[0]);
+    }
+    WriteConsonants(n[1..]);
+}
+
+string n = Console.ReadLine()!;
+WriteConsonants(n);
+
+// void WriteConsonants (string n, int i = 0)
+// {
+//     if (n.Length == 0)
+//     {
+//         return;
+//     }
+//     string vowels = "euioaEUIOA";
+//     if (!vowels.Contains(n[i]))
+//     {
+//         Console.Write(n[i]);
+//     }
+//     WriteConsonants(n.Substring(i+1,n.Length-1));
+// }
+
+// string n = Console.ReadLine()!;
+// WriteConsonants(n);
