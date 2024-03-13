72089bc7dd8fcf7cab9e523e589a9a38f0e088b2
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-13 16:55:35 +0300

    Task3

--- Desktop/ДЗ/Task3/Program.cs
+++ Desktop/ДЗ/Task3/Program.cs
@@ -2,6 +2,52 @@
 // начиная с конца. Использовать рекурсию, не использовать циклы.
 
 
+void SumNum(int [] Mas, int i = 0)
+{
+    if (i == Mas.Length) 
+    return;
+
+SumNum(Mas, i + 1);
+System.Console.Write($"{Mas[i]} ");
+}
+
+int[] Mas = new int[] {1, 2, 3, 4, 5};
+SumNum(Mas);
 
 
-int[] Mas = new int[1, 2, 3, 4, 5]
+// int[] FillArrayRandomly(int n)
+// {
+//     int[] array = new int[n];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 10);
+//     }
+//     return array;
+// }
+
+// void RecursiveArrayPrint (int [] arr, int index = 0)
+// {
+//     if (index == arr.Length)
+//     { 
+//     return;
+//     }
+//     RecursiveArrayPrint(arr, index + 1);
+//     Console.Write($"{arr[index]} ");
+  
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     foreach (var item in arr)
+//     {
+//         Console.Write($"{item} ");
+//     }    
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Введите размерность массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [] array = FillArrayRandomly(m);
+// PrintArray(array);
+// RecursiveArrayPrint(array);
+
