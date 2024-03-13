3b186bdc3e07a69494d07c9249045417ac73df37
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-03-13 15:55:51 +0300

    Task1

---
+++ Desktop/ДЗ/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int Akkerman(int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    } 
+        else 
+        
+            if(m > 0 && n == 0)
+            {
+            return Akkerman(m - 1, 1);
+            }
+        
+            else 
+            {
+                    return Akkerman(m - 1, Akkerman(m, n - 1));
+            }       
+}
+
+
+
+System.Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Result = Akkerman(m, n);
+System.Console.WriteLine(Result);
